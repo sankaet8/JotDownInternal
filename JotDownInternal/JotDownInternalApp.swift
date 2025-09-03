9efdb7737f9f8a540f9afcb7fbdd8d2ce94fd851
Sankaet Cheemalamarri <srisankaetgt@gmail.com>
2025-09-03 10:51:00 -0400

    Initial Commit

---
+++ JotDownInternal/JotDownInternalApp.swift
@@ -0,0 +1,17 @@
+//
+//  JotDownInternalApp.swift
+//  JotDownInternal
+//
+//  Created by Sankaet Cheemalamarri on 9/3/25.
+//
+
+import SwiftUI
+
+@main
+struct JotDownInternalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
