664be2eec621586129f49c3da6e8d0326ddc6758
josephmasson26 <c.p.joseph18@gmail.com>
2025-09-26 12:15:40 -0400

    Create WatchApp

---
+++ JotDownWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JotDownWatch Watch App
+//
+//  Created by Joseph Masson on 9/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JotDownWatch Watch App/JotDownWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  JotDownWatchApp.swift
+//  JotDownWatch Watch App
+//
+//  Created by Joseph Masson on 9/26/25.
+//
+
+import SwiftUI
+
+@main
+struct JotDownWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
